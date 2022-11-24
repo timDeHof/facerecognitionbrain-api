88eb4fe80af9cce68e26ad13e59db6cc7cecc9a0
timDeHof <this.is.tim.dehof@gmail.com>
2022-11-23 22:26:33 -0500

    npm dotenv

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express")
+require("dotenv").config()
+
 const bcrypt = require("bcrypt-nodejs")
 const cors = require("cors")
-const knex = require("knex")
+// const knex = require("knex")
+const { Pool } = require("pg")
 const register = require("./controllers/register")
 const signin = require("./controllers/signin")
 const image = require("./controllers/image")
 const profile = require("./controllers/profile")
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
-const db = knex({
-  client: "pg",
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  },
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: process.env.SSL,
 })
 
 const app = express()
@@ -23,12 +27,13 @@ app.use(cors())
 app.get("/", (req, res) => {
   res.send("success")
 })
-app.post("/signin", signin.handleSignin(db, bcrypt))
-app.post("/register", register.handleRegister(db, bcrypt))
-app.get("/profile/:id", profile.handleProfileGet(db))
-app.put("/image", image.handleImage(db))
+app.post("/signin", signin.handleSignin(pool, bcrypt))
+app.post("/register", register.handleRegister(pool, bcrypt))
+app.get("/profile/:id", profile.handleProfileGet(pool))
+app.put("/image", image.handleImage(pool))
 app.post("/imageurl", (req, res) => image.handleApiCall(req, res))
-// for heroku
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`app listening on port ${process.env.PORT}`)
+
+const PORT = pool.options.port || 3000
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`)
 })
