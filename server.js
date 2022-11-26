0132985981b5a0ed1d7805f1aaaab1063046e234
timDeHof <this.is.tim.dehof@gmail.com>
2022-11-25 20:27:09 -0500

    still trying to get register to work

--- server.js
+++ server.js
@@ -36,36 +36,6 @@ app.get("/", (req, res) => {
 })
 app.post("/signin", signin.handleSignin(db, bcrypt))
 app.post("/register", (req, res) => {
-  const { email, name, password } = req.body
-  if (!email || !password || !name) {
-    return res.status(400).json("incorrect form submission")
-  }
-  const hash = bcrypt.hashSync(password)
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0])
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-  })
-    .catch((err) => res.status(400))
-    .json("unable to register")
   // const { email, name, password } = req.body
   // db("users")
   // returning("*.")
@@ -78,7 +48,7 @@ app.post("/register", (req, res) => {
   //   res.json(user[0])
   // })
   // .catch((err) => res.status(400).json("unable to join"))
-  // register.handleRegister(req, res, db, bcrypt)
+  register.handleRegister(req, res, db, bcrypt)
 })
 app.get("/profile/:id", (req, res) => {
   profile.handleProfileGet(req, res, db)
