8ba8d78d9ed46759ee862096654aeca2ee68a92a
timDeHof <this.is.tim.dehof@gmail.com>
2023-01-12 13:45:31 -0500

    update model id

--- controllers/image.js
+++ controllers/image.js
@@ -8,7 +8,7 @@ const handleApiCall = (req, res) => {
   stub.PostModelOutputs(
     {
       // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
-      model_id: "a403429f2ddf4b49b307e318f00e528b",
+      model_id: "6dc7e46bc9124c5c8824be4822abe105",
       inputs: [{ data: { image: { url: req.body.input } } }],
     },
     metadata,
